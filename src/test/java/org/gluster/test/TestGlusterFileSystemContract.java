2b218e4308b80c1491da0bbab8ce9ed3f6fa8116
jayunit100 <jayunit100@gmail.com>
2013-09-10 13:53:17 -0400

    remove mkdirs

--- src/test/java/org/gluster/test/TestGlusterFileSystemContract.java
+++ src/test/java/org/gluster/test/TestGlusterFileSystemContract.java
@@ -48,6 +48,7 @@ public class TestGlusterFileSystemContract extends FileSystemContractBaseTest {
               "testRenameFileAsExistingFile",
               "testWorkingDirectory",
               "testListStatus",
+              "testMkdirs",
               "testRenameDirectoryMoveToNonExistentDirectory",
               "testRenameDirectoryAsExistingDirectory",
               "testRenameDirectoryAsNonExistentDirectory"
