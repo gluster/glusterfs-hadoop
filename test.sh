1550a93793fe9d3e04ea116e0b645d8c12e5f093
childsb <bchilds@redhat.com>
2013-11-01 13:12:35 -0600

    update test.sh with correct package name

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@
 # this could be conditionally set.  it may be set higher up as part of the global env variables.
 rm -rf target/
 export GLUSTER_MOUNT=/mnt/glusterfs
-export HCFS_FILE_SYSTEM_CONNECTOR=org.apache.hadoop.hcfs.test.connector.glusterfs.GlusterFileSystemTestConnector
+export HCFS_FILE_SYSTEM_CONNECTOR=org.apache.hadoop.fs.test.connector.glusterfs.GlusterFileSystemTestConnector
 export HCFS_CLASSNAME=org.apache.hadoop.fs.glusterfs.GlusterFileSystem
 rm -rf target/
 
